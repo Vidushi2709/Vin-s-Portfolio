b145ce8c082752759a2f49aaf5f35087356598a1
Vidushi anand <Vidushi2709@gmail.com>
2025-03-25 00:48:56 +0530

    fixit

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ col1, col2 = st.columns([1, 2])
 
 with col1:
     st.markdown("<div style='margin-top: 50px;'></div>", unsafe_allow_html=True) 
-    st.image("C:/devdev/Vin's_portfolio/vin.jpg", width= 300)
+    st.image("vin.jpg", width= 300)
 
 with col2:
     st.markdown(
